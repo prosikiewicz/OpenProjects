7d32f74848c4ef5f3a803be2097510441d56aa8f
prosikiewicz <pawel.rosikiewicz@swissai.org>
2019-02-24 13:11:20 +0100

    plot nr2 has ben commented

--- Simple galaxy plot.R
+++ Simple galaxy plot.R
@@ -6,14 +6,14 @@ data_x <- sort(c(rnorm(n = 1000, mean = 10, sd = 2)))
 data_y <- sapply(data_x, function(x){x+rnorm(1, mean=0, sd=2)})
 
 # two plots on one page
-par(mfrow=c(1,3))
+par(mfrow=c(1,2))
 
 # plot 1
 plot(data_x, data_y, pch=16, cex=0.4, ylim=c(0,20), xlim=c(0,20))
 points(data_x, data_x, pch=16, cex=0.5, col="red")
 
 #plot 2
-plot(data_x, c(1:1000), pch=16, cex=0.4)
+# plot(data_x, c(1:1000), pch=16, cex=0.4)
 
 # plot 3
 plot(data_x, data_y, pch=16, cex=0.4, ylim=c(0,20), xlim=c(0,20), col="red")
